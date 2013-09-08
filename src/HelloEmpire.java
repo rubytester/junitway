c107fb00b3f634823c34bd5e21a36efb191cb258
rubytester <git@rubytester.com>
2013-09-07 19:37:08 -0500

    examples continued

--- src/HelloEmpire.java
+++ src/HelloEmpire.java
@@ -1,8 +1,46 @@
-import org.junit.Test;
-
 public class HelloEmpire {
 
+    //field with default value
+    Boolean someFieldForClass = false;
+    // also field with default null
+    String s = null;
+
+    // constructor that sets field to value other than default
+    public HelloEmpire() {
+        someFieldForClass = true;
+    }
+
+    // constructor accepts String and sets field to value other than default
+    private HelloEmpire(String s) {
+        this.s = s;
+    }
+
+    // public method of the class instance
+    // this is a happy way of sending messages to objects
     public String hello() {
         return "Hello";
+    }
+
+    // static method means it can be called on Class (like class method in Ruby)
+    //it's like a function call, requires no state so static is fine
+    public static Boolean exists(boolean b) {
+        return b ? Boolean.TRUE : Boolean.FALSE;
+    }
+
+
+
+    // so static factory method sets this? but how?
+    private static HelloEmpire rome = null;
+
+    // static method acts as factory method returns instance of class
+    public static HelloEmpire createRome() {
+        if (rome == null) {
+            rome = new HelloEmpire("AVE CESAR");
+        }
+        return rome;
+    }
+
+    public String salutation() {
+        return s;
     }
 }
--- src/HelloEmpireTest.java
+++ src/HelloEmpireTest.java
@@ -1,13 +1,50 @@
 import org.junit.Test;
 
+import static org.junit.Assert.*; // we want all assertion
+
 public class HelloEmpireTest {
 
     @Test
-    public void testHello(){
+    public void helloPublicMethod() {
         HelloEmpire he = new HelloEmpire();
+        // we know hello returns String object
         String check = he.hello();
-        assert check.equalsIgnoreCase("HelloBob");
+        assert check.equalsIgnoreCase("Hello");
+    }
 
+    @Test
+    public void fieldValue() {
+        HelloEmpire he = new HelloEmpire();
+        //accessing field here not method
+        assertTrue(he.someFieldForClass);
+    }
+
+    @Test
+    public void staticMethodExample() {
+        //example of static
+        assertEquals(HelloEmpire.exists(true), Boolean.TRUE);
+
+        HelloEmpire rome = HelloEmpire.createRome();
+        assertEquals(rome.salutation(), "AVE CESAR");
+    }
+
+    @Test
+    public void stringEquality() {
+
+        assertTrue("Hello" == "Hello"); //Strings equal
+
+        String s = new String("H");
+        String t = new String("H");
+
+        assertFalse(s == t); //but string objects not
+    }
+
+    @Test
+    public void singleInstance() {
+        //static method factory returns the same object every time
+        HelloEmpire he = HelloEmpire.createRome();
+        HelloEmpire he2 = HelloEmpire.createRome();
+        assertTrue(he == he2);
     }
 
 }
